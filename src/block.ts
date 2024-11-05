272557ba96ec477dbdec8a16820feed2547ea2af
João Figueiredo <joao.ggwp@outlook.com>
2024-11-05 07:42:32 -0300

    feat: add new validation in `block`
      and fix tests broken

--- src/block.ts
+++ src/block.ts
@@ -19,12 +19,12 @@ export default class Block {
     return sha256(this.index + this.data + this.timestamp + this.previousHash).toString()
   }
 
-  isValid():boolean{
-    if(this.index < 0) return false;
+  isValid(previusHash: string, previusIndex:number):boolean{
+    if(previusIndex !== this.index -1) return false;
     if(!this.hash) return false;
     if(!this.data) return false;
     if(this.timestamp < 1) return false;
-    if(!this.previousHash) return false;
+    if(this.previousHash !== previusHash) return false;
     return true
   }
 }
