7ad9b040b267fa7debea075cb72efab01ab2743a
João Figueiredo <joao.ggwp@outlook.com>
2024-11-05 07:43:08 -0300

    feat: create method to getLast block and add block in blockchain
      and create tests

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,9 +1,34 @@
-import Block from "./block";
+import Block from "./block"
 
 export default class BlockChain {
   blocks: Block[]
+  nextIndex: number = 0
 
   constructor(){
-    this.blocks = [new Block(0, "", "Genesis Block")]
+    this.blocks = [new Block(this.nextIndex, "", "Genesis Block")]
+    this.nextIndex++
+  };
+
+  getLastBlock():Block {
+    return this.blocks[this.blocks.length -1]
+  };
+
+  addBlock(block: Block): boolean {
+    const lastBlock = this.getLastBlock()
+    if (!block.isValid(lastBlock.hash, lastBlock.index)) return false
+    this.blocks.push(block)
+    this.nextIndex ++
+    return true
+  };
+
+  isValid(): boolean {
+    for (let i = this.blocks.length -1; i > 0; i--){
+      const currentBlock = this.blocks[i];
+      const previusBlock = this.blocks[i -1];
+      const isValid = currentBlock.isValid(previusBlock.hash, previusBlock.index)
+      if(!isValid) return false
+    }
+    return true
   }
+
 }
