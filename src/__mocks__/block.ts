8aff2eac2ee04295f1b672c4953938abe739b699
João Figueiredo <joao.ggwp@outlook.com>
2025-04-21 21:42:02 -0300

    fix: ajust mock files and tests 100 coverage

--- src/__mocks__/block.ts
+++ src/__mocks__/block.ts
@@ -1,3 +1,4 @@
+import BlockInfo from "../blockinfo";
 import Validation from "../lib/validation";
 
 export default class Block {
@@ -23,5 +24,13 @@ export default class Block {
     if (!previusHash || previusIndex < 0 || this.index < 0)
       return new Validation(false, "Invalid mock block.");
     return new Validation();
+  }
+
+  static fromBlockInfo(blockInfo: BlockInfo): Block {
+    const block = new Block();
+    block.index = blockInfo.index;
+    block.previousHash = blockInfo.previousHash;
+    block.data = blockInfo.data;
+    return block;
   }
 }
